eb28743b378c9e6f07299bd18f14178c6ee6f56e
IgnacioGalceran <igna.galceran@gmail.com>
2024-08-11 21:01:48 -0300

    agrego validaciones para turnos

--- src/entities/turnos/turnos.routes.ts
+++ src/entities/turnos/turnos.routes.ts
@@ -3,13 +3,35 @@ import { add, findAll, findOne, update, remove } from "./turnos.controller.js";
 import sanitizeTurnoInput from "./turnos.middleware.js";
 import { verifyToken } from "../../auth/auth.middleware.js";
 import checkPermissions from "../../shared/checkPermissions.js";
+import { validateInput } from "./turnos.validations.js";
 
 export const router = express.Router();
 
 router
   .get("/", verifyToken, checkPermissions, findAll)
-  .get("/:id", verifyToken, checkPermissions, findOne)
-  .post("/", verifyToken, checkPermissions, sanitizeTurnoInput, add)
-  .put("/:id", verifyToken, checkPermissions, sanitizeTurnoInput, update)
-  .patch("/:id", verifyToken, checkPermissions, sanitizeTurnoInput, update)
-  .delete("/:id", verifyToken, checkPermissions, remove);
+  .get("/:id", verifyToken, checkPermissions, validateInput, findOne)
+  .post(
+    "/",
+    verifyToken,
+    checkPermissions,
+    validateInput,
+    sanitizeTurnoInput,
+    add
+  )
+  .put(
+    "/:id",
+    verifyToken,
+    checkPermissions,
+    validateInput,
+    sanitizeTurnoInput,
+    update
+  )
+  .patch(
+    "/:id",
+    verifyToken,
+    checkPermissions,
+    validateInput,
+    sanitizeTurnoInput,
+    update
+  )
+  .delete("/:id", verifyToken, checkPermissions, validateInput, remove);
---
+++ src/entities/turnos/turnos.validations.ts
@@ -0,0 +1,56 @@
+import { NextFunction, Request, Response } from "express";
+import { ObjectId } from "@mikro-orm/mongodb";
+import { InvalidFields, InvalidId } from "../../shared/errors.js";
+import Joi from "joi";
+
+const isValidObjectId = (value: string) => {
+  return ObjectId.isValid(value);
+};
+
+const especialidades = Joi.object({
+  fecha: Joi.date().required(),
+  medico: Joi.string()
+    .custom((value, helpers) => {
+      if (!isValidObjectId(value)) {
+        return helpers.error("custom");
+      }
+      return value;
+    })
+    .messages({
+      custom:
+        "El id del medico debe ser un ObjectId válido (24 caracteres hexadecimales).",
+    }),
+  paciente: Joi.string()
+    .custom((value, helpers) => {
+      if (!isValidObjectId(value)) {
+        return helpers.error("custom");
+      }
+      return value;
+    })
+    .messages({
+      custom:
+        "El id del paciente debe ser un ObjectId válido (24 caracteres hexadecimales).",
+    }),
+});
+
+export const validateInput = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.params.id) {
+    if (!isValidObjectId(req.params.id)) {
+      next(new InvalidId());
+    }
+  }
+
+  if (Object.keys(req.body).length) {
+    const { error } = especialidades.validate(req.body);
+
+    if (error) {
+      next(new InvalidFields(error.details[0].message));
+    }
+  }
+
+  next();
+};
